21dbd77cf56a480f87f8c5dfa267d206528a511f
Elisabeth0101 <sime2005@usherbrooke.ca>
2025-11-25 15:14:40 -0500

    code final app4

--- racecar-ros2/racecar_autopilot/racecar_autopilot/slash_controller.py
+++ racecar-ros2/racecar_autopilot/racecar_autopilot/slash_controller.py
@@ -28,16 +28,21 @@ class SlashController(Node):
         self.dt = 0.05
         self.timer = self.create_timer(self.dt, self.timed_controller)
 
-        # Paramters
+        # Parameters
 
         # Controller
         self.steering_offset = 0.0  # To adjust according to the vehicle
+        
+        # K_autopilot is 2x3 matrix: [propulsion_gains; steering_gains]
         self.K_autopilot = np.array([
-            [0, 0, 8.22200412],
-            [0.31622777, 0.53827192, 0]
+            [0, 0, 1.0],                      # V control gains [y, theta, v]
+            [0.3162277, -0.53827192, 0]       # delta control gains [y, theta, v]
         ])
 
-        self.K_parking = None  # TODO: DESIGN PLACEMENT DE POLES
+        self.K_parking = np.array([
+          [1, -0.00008306, -0.000166137],                      # V control gains
+          [0, 0.2, -0.32]       # delta control gains (K2)
+        ])
 
         self.Rm = 0.64
         self.km = 0.0028
@@ -61,12 +66,12 @@ class SlashController(Node):
         self.steering_ref = 0
         self.high_level_mode = 0  # Control mode of this controller node
 
-        # Ouput commands
+        # Output commands
         self.propulsion_cmd = 0  # Command sent to propulsion
         self.arduino_mode = 0  # Control mode
         self.steering_cmd = 0  # Command sent to the steering servo
 
-        # Sensings inputs
+        # Sensing inputs
         self.laser_y = 0
         self.laser_theta = 0
         self.velocity = 0
@@ -93,7 +98,7 @@ class SlashController(Node):
 
         else:
 
-            # APP2 (open-loop steering) Controllers Bellow
+            # APP2 (open-loop steering) Controllers Below
             if self.high_level_mode == 1:
                 # Open-Loop propulsion and steering
                 self.propulsion_cmd = self.propulsion_ref
@@ -104,7 +109,7 @@ class SlashController(Node):
             elif self.high_level_mode == 0:
                 # Closed-loop velocity on arduino, open-loop steering
                 self.propulsion_cmd = self.propulsion_ref
-                self.arduino_mode = 2
+                self.arduino_mode = 1
                 self.steering_cmd = self.propulsion_ref + self.steering_offset
 
             elif self.high_level_mode == 2:
@@ -113,52 +118,36 @@ class SlashController(Node):
                 self.arduino_mode = 3
                 self.steering_cmd = self.steering_ref + self.steering_offset
 
-            # APP4 (closed-loop steering) controllers bellow
+            # APP4 (closed-loop steering) controllers below
             elif self.high_level_mode == 3 or self.high_level_mode == 5:
                 # Closed-loop velocity and steering
 
-                #########################################################
-                # TODO: COMPLETEZ LE CONTROLLER
-
-                # Auto-pilot # 1
-
-                # x = [ ?,? ,.... ]
-                # r = [ ?,? ,.... ]
-                # u = [ servo_cmd , prop_cmd ]
-
+                # State vector: [lateral_position, heading_angle, velocity]
                 x = [self.laser_y, self.laser_theta, self.velocity]
-                r = [0, 0, 2] 
+                # Reference: [desired_y, desired_theta, desired_velocity]
+                r = [-0.2, 0, 2] 
 
+                # Get control commands
                 u = self.controller1(x, r)
 
+                # Apply commands
                 self.steering_cmd = u[1] + self.steering_offset
                 self.propulsion_cmd = u[0]
-                self.arduino_mode = 1  # Mode ??? on arduino
-                # TODO: COMPLETEZ LE CONTROLLER
-                #########################################################
+                self.arduino_mode = 2  # Open-loop mode on arduino
 
             elif self.high_level_mode == 4:
                 # Closed-loop position and steering
-
-                #########################################################
-                # TODO: COMPLETEZ LE CONTROLLER
-
-                # Auto-pilot # 1
-
-                # x = [ ?,? ,.... ]
-                # r = [ ?,? ,.... ]
-                # u = [ servo_cmd , prop_cmd ]
-
-                x = None
-                r = None
+		
+		#self.get_logger().debug("In high level mode 4")
+                # TODO: Define state and reference for parking controller
+                x = [self.position, self.laser_y, self.laser_theta]
+                r = [4, -0.2, 0]  # Adjust reference as needed
 
                 u = self.controller2(x, r)
 
                 self.steering_cmd = u[1] + self.steering_offset
                 self.propulsion_cmd = u[0]
-                self.arduino_mode = 0  # Mode ??? on arduino
-                # TODO: COMPLETEZ LE CONTROLLER
-                #########################################################
+                self.arduino_mode = 5  # Mode on arduino
 
             elif self.high_level_mode == 6:
                 # Reset encoders
@@ -174,34 +163,55 @@ class SlashController(Node):
 
             elif self.high_level_mode == 8:
                 # Template for custom controllers
-
                 self.steering_cmd = 0 + self.steering_offset
                 self.propulsion_cmd = 0
-                self.arduino_mode = 0  # Mode ??? on arduino
+                self.arduino_mode = 0
 
         self.send_arduino()
 
-    def control_law(x, K, r):
-
+    #######################################
+    def control_law(self, x, K, r):
+        """
+        Compute control law u = -K(x - r) with saturation
+        
+        Args:
+            x: State vector [y, theta, v]
+            K: Control gain matrix (2x3)
+            r: Reference vector [y_ref, theta_ref, v_ref]
+            
+        Returns:
+            V: Propulsion voltage command (saturated)
+            delta: Steering angle command (saturated)
+        """
         V_sat = 8.4
         delta_sat = 0.5
 
-        """
-        Calcule u = -K (x - x_ref) et applique saturations.
-        x: vecteur état [y, theta, v]
-        K: matrice 2x3
-        retourne: V, delta (après saturation)
-        """
-        x_tilde = x - r  # erreur par rapport à la consigne
-        u = -K @ x_tilde     # commande continue
+        x_tilde = x - r  # Error from reference
+        u = -K @ x_tilde  # Control command
+        
         V = float(u[0])
         delta = float(u[1])
-        # saturations
-        V = np.clip(V + 6.5, 0, V_sat)
+        
+        # Apply saturations
+        V = np.clip(V+1.4, 0, V_sat)
         delta = np.clip(delta, -delta_sat, delta_sat)
+        
         return V, delta
-    
-    def sign_smooth(x):
+
+    def control_law2(self, x, K, r):
+        delta_sat = 0.5
+
+        x_tilde = x - r  # erreur par rapport à la consigne
+        u = -K @ x_tilde     # commande continue
+        vd = float(u[0])
+        delta = float(u[1])
+        # saturations
+        delta = np.clip(delta, -delta_sat, delta_sat)
+        return vd, delta
+
+    #######################################
+    def sign_smooth(self, x):
+        """Smooth sign function"""
         if x == 0:
             return 0
         elif x > 0:
@@ -211,83 +221,89 @@ class SlashController(Node):
 
     #######################################
     def controller1(self, y, r):
+        """
+        Autopilot controller for velocity and steering
+        
+        Args:
+            y: State vector [y_position, theta, velocity]
+            r: Reference vector [y_ref, theta_ref, v_ref]
+            
+        Returns:
+            u: Control vector [V_command, delta_command]
+        """
+        # Convert to numpy arrays
+        x = np.array(y)
+        r_vec = np.array(r)
 
-        # Control Law TODO
+        # Compute closed-loop control
+        V, delta = self.control_law(x, self.K_autopilot, r_vec)
 
-        y, theta, v = y
-
-        # commande (fermée)
-        V, delta = self.control_law(y, self.K_autopilot, r)
-
-        u = np.array([V, delta])  # placeholder
+        u = np.array([V, delta])
 
         return u
 
     #######################################
     def controller2(self, y, r):
 
-        # Control Law TODO
+        x = np.array(y)
+        r_vec = np.array(r)
 
-        u = np.array([0, 0])  # placeholder
+        # commande (fermée)
+        vd, delta = self.control_law2(x, self.K_parking, r_vec)
 
-        # u = self.K_parking  @ (r - x)
+        u = np.array([vd, delta])
 
         return u
-
     #######################################
     def read_ref(self, ref_msg):
-
-        # Read received references
+        """Read reference commands from topic"""
         self.propulsion_ref = ref_msg.linear.x
         self.high_level_mode = ref_msg.linear.z
         self.steering_ref = ref_msg.angular.z
 
     #######################################
     def read_laser(self, msg):
-
+        """Read laser/position sensor data"""
         self.laser_y = msg.linear.y
         self.laser_theta = msg.angular.z
 
     #######################################
     def read_arduino(self, msg):
-
-        # Read feedback from arduino
+        """Read feedback from arduino (encoders)"""
         self.velocity = msg.data[1]
         self.position = msg.data[0]
 
-    ##########################################################################################
+    #######################################
     def send_arduino(self):
-
-        # Init encd_info msg
+        """Send commands to arduino"""
+        # Init command msg
         cmd_prop = Twist()
 
-        # Msg
-        cmd_prop.linear.x = float(self.propulsion_cmd)  # Command sent to propulsion
+        # Package commands
+        cmd_prop.linear.x = float(self.propulsion_cmd)  # Propulsion command
         cmd_prop.linear.z = float(self.arduino_mode)  # Control mode
+        cmd_prop.angular.z = float(self.steering_cmd)  # Steering command
 
-        cmd_prop.angular.z = float(
-            self.steering_cmd
-        )  # Command sent to the steering servo
-
-        # Publish cmd msg
+        # Publish command
         self.pub_cmd.publish(cmd_prop)
 
     #######################################
     def pub_kinematic(self):
-        # init encd_info msg
+        """Publish kinematic information (unused)"""
+        # init msg
         pos = Twist()
         vel = Twist()
         acc = Twist()
 
-        # Msg
+        # Set values
         pos.linear.x = 0
         vel.linear.x = 0
         acc.linear.x = 0
 
-        # Publish cmd msg
-        self.pub_pos.publish(pos)
-        self.pub_vel.publish(vel)
-        self.pub_acc.publish(acc)
+        # Publish (note: publishers not initialized for this)
+        # self.pub_pos.publish(pos)
+        # self.pub_vel.publish(vel)
+        # self.pub_acc.publish(acc)
 
 
 def main(args=None):
@@ -297,8 +313,6 @@ def main(args=None):
     node.destroy_node()
     rclpy.shutdown()
 
-
-#########################################
 
 if __name__ == "__main__":
     main()
